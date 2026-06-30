7ed08f244d32eaee508a239d862169008a0dbdde
Tanishq-2004-ops <Tanishq.15883@stu.upes.ac.in>
2026-06-30 21:50:44 +0530

    Fourth commit

---
+++ day4_reversearray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class day4_reversearray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter elements of array: ");
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){ 
+            arr[i] = sc.nextInt();
+        }
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        System.out.println("Reversed array:");
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
