fd9163ba2d3e7b23d817d531a7076861d3e85ceb
Tanishq-2004-ops <Tanishq.15883@stu.upes.ac.in>
2026-09-17 10:45:04 +0530

    82nd commit

---
+++ day82_SelectionSort.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class day82_SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sort the array using selection sort
+        selectionSort(arr);
+
+        // Output the sorted array
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
